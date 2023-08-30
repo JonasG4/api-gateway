f6f04d904bc10ceb55cddc806c4c0dc3c74bb36d
paizcode <josuepaiz487@gmail.com>
2023-08-29 21:59:06 -0600

    Agregando ClientProxy para candidatos politicos

--- src/common/proxy/client-proxy.ts
+++ src/common/proxy/client-proxy.ts
@@ -20,4 +20,14 @@ export class ClientProxyAppAdminitracion {
       },
     });
   }
+
+  clientProxyCandidatosPoliticos(): ClientProxy {
+    return ClientProxyFactory.create({
+      transport: Transport.RMQ,
+      options: {
+        urls: this.config.get('AMQP_URL'),
+        queue: RabbitMQ.CandidatosPoliticosQueue
+      }
+    })
+  }
 }
